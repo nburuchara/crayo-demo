0bd3e836d7a035ad5fd8b98e0bce34efa71888ee
Norman Buruchara <normanburuchara90@gmail.com>
2024-08-09 18:19:57 +0300

    [wrapping up]: wrapping up demo

--- src/dashboard/dash-helper-files/Search-Terms.js
+++ src/dashboard/dash-helper-files/Search-Terms.js
@@ -21,12 +21,6 @@ const results = [
     {"id": "998573", "name": "AI Avatars", "category": "Apps", "subCat1": "Popular Apps", "page": "Event types", "lastCat": "top"},
     {"id": "998573", "name": "Voiceover Story", "category": "Apps", "subCat1": "Popular Apps", "page": "Event types", "lastCat": "top"},
     {"id": "998573", "name": "Download YouTube Videos", "category": "Apps", "subCat1": "Popular Apps", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "All Apps", "category": "Apps", "subCat1": "Popular Apps", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Create splitscreen content w/ our gameplay library.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Splitscreen Video", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Create AI avatars to say anything for UGC content.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "AI Avatars", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Create wholesome, scary, or any type of story using AI voices.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Voiceover Story", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Get YouTube videos in MP4 format directly in Crayo.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Download YouTube Videos", "page": "Event types", "lastCat": "top"},
-
 
     {"id": "998573", "name": "Main Editor", "category": "Apps", "subCat1": "Dashboard", "page": "Event types", "lastCat": "top"},
     {"id": "998573", "name": "My Assets", "category": "Apps", "subCat1": "Dashboard", "page": "Event types", "lastCat": "top"},
@@ -77,15 +71,6 @@ const results = [
     {"id": "998573", "name": "AI Voiceover Video Guide", "category": "Learning", "page": "Event types", "lastCat": "top"},
     {"id": "998573", "name": "YouTube Downloader Guide", "category": "Learning", "page": "Event types", "lastCat": "top"},
 
-    {"id": "998573", "name": "Learn how to create AI-generated video for your stories.", "category": "Learning", "subCat1": "AI Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to make high-quality voice-overs for your videos.", "category": "Learning", "subCat1": "Voiceover Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to create ChatGPT-like image videos.", "category": "Learning", "subCat1": "ChatGPT Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to create splitscreen content using our gameplay library.", "category": "Learning", "subCat1": "Split Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to create simulated text conversation videos for Instagram, Tiktok and Shorts.", "category": "Learning", "subCat1": "Texts Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to create AI avatars with fully customized speech for UGC content.", "category": "Learning", "subCat1": "AI Avatar Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to add any type of AI voiceover from our vast library to your videos.", "category": "Learning", "subCat1": "AI Voiceover Video Guide", "page": "Event types", "lastCat": "top"},
-    {"id": "998573", "name": "Learn how to add any YouTube video directly into your content via Crayo.", "category": "Learning", "subCat1": "YouTube Downloader Guidee", "page": "Event types", "lastCat": "top"},
-
 
     {"id": "998573", "name": "blur vid sample", "category": "Projects", "subCat1": "Blur Video", "page": "Event types", "lastCat": "top"},
     {"id": "998573", "name": "ChatGPT NEW tester", "category": "Projects", "subCat1": "ChatGPT Video", "page": "Event types", "lastCat": "top"},
@@ -99,6 +84,19 @@ const results = [
     {"id": "998573", "name": "voiceover testing 3", "category": "Projects", "subCat1": "Voiceover Video", "page": "Event types", "lastCat": "top"},
 
 
+    {"id": "998573", "name": "Create splitscreen content w/ our gameplay library.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Splitscreen Video", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Create AI avatars to say anything for UGC content.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "AI Avatars", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Create wholesome, scary, or any type of story using AI voices.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Voiceover Story", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Get YouTube videos in MP4 format directly in Crayo.", "category": "Apps", "subCat1": "Popular Apps", "subCat2": "Download YouTube Videos", "page": "Event types", "lastCat": "top"},
+
+    {"id": "998573", "name": "Learn how to create AI-generated video for your stories.", "category": "Learning", "subCat1": "AI Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to make high-quality voice-overs for your videos.", "category": "Learning", "subCat1": "Voiceover Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to create ChatGPT-like image videos.", "category": "Learning", "subCat1": "ChatGPT Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to create splitscreen content using our gameplay library.", "category": "Learning", "subCat1": "Split Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to create simulated text conversation videos for Instagram, Tiktok and Shorts.", "category": "Learning", "subCat1": "Texts Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to create AI avatars with fully customized speech for UGC content.", "category": "Learning", "subCat1": "AI Avatar Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to add any type of AI voiceover from our vast library to your videos.", "category": "Learning", "subCat1": "AI Voiceover Video Guide", "page": "Event types", "lastCat": "top"},
+    {"id": "998573", "name": "Learn how to add any YouTube video directly into your content via Crayo.", "category": "Learning", "subCat1": "YouTube Downloader Guidee", "page": "Event types", "lastCat": "top"},
   
 
 ]
